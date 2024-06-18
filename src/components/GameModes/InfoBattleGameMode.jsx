ef69909931cd07c6521098ed38a9d39eb3f79e5f
Almir Muminovic <projectalmir@gmail.com>
2024-06-19 00:58:27 +0200

    format: new text for home page

--- src/components/GameModes/InfoBattleGameMode.jsx
+++ src/components/GameModes/InfoBattleGameMode.jsx
@@ -5,7 +5,7 @@ const InfoBattleGameMode = () => {
         Please select two options and press battle. You must select all
         champions from your pool of champions. If there is not enough champions
         in your pool, you can select remaining champions from different pools
-        excluding enemy pool. Feel free to play Blind/Draft mode. Depending on pool of champs you can decide if you want to involve bans.
+        excluding enemy pool. Feel free to play Blind/Draft mode. Depending on pool of champions you can decide if you want to involve bans.
       </p>
     </>
   );
